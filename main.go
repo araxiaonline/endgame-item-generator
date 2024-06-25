37d5c2eeaf9136bacaf80a2007530e00b22977fd
Ben Carter <ben.carter.dev@gmail.com>
2024-06-24 23:45:07 -0400

    Added tests around getting loot for a map boss

--- main.go
+++ main.go
@@ -20,10 +20,33 @@ func main() {
 	}
 
 	for _, boss := range bosses {
-		log.Println(boss)
+
+		log.Printf("Getting loot for Boss: %s\n", boss.Name)
+
+		items, err := models.DB.GetBossLoot(boss.Entry)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, item := range items {
+			log.Println(item)
+		}
+
 	}
 
 	defer models.DB.Close()
+
+	// theItem := models.Item{}
+	// s := reflect.ValueOf(&theItem).Elem()
+	// numCols := s.NumField()
+	// log.Println(numCols)
+	// columns := make([]interface{}, numCols)
+	// fmt.Print(columns)
+	// for i := 0; i < numCols; i++ {
+	// 	field := s.Field(i)
+	// 	columns[i] = field.Addr().Interface()
+	// }
+	// fmt.Println(columns...)
 
 	// var rows *sql.Rows
 	// rows, err = DB.Query("SELECT name, entry FROM item_template where name like 'Hypnotic B%';")
