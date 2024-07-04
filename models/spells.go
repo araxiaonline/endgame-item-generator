b50106d15f4d3a24ecd6cf757cc0ff97e244435a
Ben Carter <ben.carter.dev@gmail.com>
2024-07-04 13:07:50 -0400

    Added conversion functions to spells

--- models/spells.go
+++ models/spells.go
@@ -2,62 +2,96 @@ package models
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/araxiaonline/endgame-item-generator/utils"
 	"github.com/thoas/go-funk"
 )
 
+// list of spell effects that require scaling
 var SpellEffects = [...]int{
-	2,   // School Damage
-	6,   // AppyAura
-	9,   // HealthLeech
-	10,  // Heal
-	17,  // WeaponDamage
-	22,  // Parry
-	23,  // Block
-	31,  // WeaponDmg % Increase
-	35,  // Apply Area Aura
-	58,  // Weapon Damange Increase
-	67,  // Max Health
-	121, // Normalized Weapon Damage
+	2,  // School Damage
+	6,  // AppyAura
+	9,  // HealthLeech
+	10, // Heal
+	30, // Restores Mana
+	35, // Apply Area Aura
 }
 
+// list of spell aura effects that require scaling
 var SpellAuraEffects = [...]int{
 	3,   // DOT
 	8,   // HOT
-	13,  // Modifies Damage Done
-	14,  // Modifies Damange Taken
+	13,  // Modifies Spell Damage Done
 	15,  // Modifies Damage Shield
 	22,  // Modifies Resistance
-	29,  // Modifies a Stage
 	34,  // Modifies HEalth
-	85,  // Modifies Power Regen
+	85,  // Modifies Mana Regen
 	99,  // Modifies Attack Power
-	107, // Modifier Flat
-	115, // Modifies Healing
 	124, // Modifies Range Attack Power
-	127, // Melee Attack Bonus Dmg
 	135, // Modifies Healing Done
-	189, // Modifies Armor Rating
+	189, // Modifies Critical Strike
 }
 
-var SpellEffectStatMap = map[int][]int{
-	10: {41},
-	17: {38, 39},
-	22: {14},
-	23: {15},
-	58: {38, 39},
-	67: {1},
+var AuraEffectsStatMap = map[int]int{
+	8:   46,
+	13:  42,
+	85:  43,
+	99:  38,
+	124: 39,
+	135: 45,
+}
+
+// Usually in EffectMiscValueA to describe what the Aura modifies
+var SpellModifiers = [...]int{
+	0,  // damage
+	1,  // duration
+	2,  // threat
+	3,  // effect1
+	4,  // charges
+	5,  // range
+	6,  // radius
+	7,  // crit chance
+	8,  // all effects
+	9,  // No pushback
+	10, // Cast Time
+	11, // CD
+	12, // effect2
+	13, // ignore armor
+	14, // cost
+	15, // crit damage bonus
+	16, // resist miss chance
+	17, // jump targets
+	18, // Chance of success
+	19, // Amplitude
+	20, // Dmg multiplier
+	21, // GCD
+	22, // DoT
+	23, // effect3
+	24, // bonus multiplier
+	26, // PPM
+	27, // value multiplier
+	28, // resist dispel chance
+	29, // crit damage bonus 2
 }
 
 // result of a stat conversion from spell to raw stats on item
-type SpellStat struct {
+type ConvItemStat struct {
 	StatType  int
 	StatValue int
 	Budget    int
 }
 
+// Spell Effect with max value for effect storage
+type SpellEffect struct {
+	Effect        int
+	BasePoints    int
+	DieSides      int
+	CalculatedMax int
+}
+
+// DB Mapping from spell_dbc
 type Spell struct {
 	ID                        int    `db:"ID"`
 	Name                      string `db:"Name_Lang_enUS"`
@@ -102,6 +136,70 @@ func (db Database) GetSpell(id int) (Spell, error) {
 	return spell, nil
 }
 
+func calcMaxValue(base int, sides int) int {
+	if base < 0 {
+		return base - sides
+	}
+
+	return base + sides
+}
+
+func (s Spell) GetSpellEffects() []SpellEffect {
+	effects := make([]SpellEffect, 0)
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.Effect1,
+		BasePoints:    s.EffectBasePoints1,
+		DieSides:      s.EffectDieSides1,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints1, s.EffectDieSides1),
+	})
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.Effect2,
+		BasePoints:    s.EffectBasePoints2,
+		DieSides:      s.EffectDieSides2,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints2, s.EffectDieSides2),
+	})
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.Effect3,
+		BasePoints:    s.EffectBasePoints3,
+		DieSides:      s.EffectDieSides3,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints3, s.EffectDieSides3),
+	})
+
+	return effects
+}
+
+// Get he effects and calculate the max value for the a
+func (s Spell) GetAuraEffects() []SpellEffect {
+	effects := make([]SpellEffect, 0)
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.EffectAura1,
+		BasePoints:    s.EffectBasePoints1,
+		DieSides:      s.EffectDieSides1,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints1, s.EffectDieSides1),
+	})
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.EffectAura2,
+		BasePoints:    s.EffectBasePoints2,
+		DieSides:      s.EffectDieSides2,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints2, s.EffectDieSides2),
+	})
+
+	effects = append(effects, SpellEffect{
+		Effect:        s.EffectAura3,
+		BasePoints:    s.EffectBasePoints3,
+		DieSides:      s.EffectDieSides3,
+		CalculatedMax: calcMaxValue(s.EffectBasePoints3, s.EffectDieSides3),
+	})
+
+	return effects
+}
+
+// this spell effect has stats or effects that need to be scaled
 func (s Spell) effectNeedsScaled() bool {
 	if s.Effect1 == 0 {
 		return false
@@ -115,6 +213,7 @@ func (s Spell) effectNeedsScaled() bool {
 	return false
 }
 
+// this aura effect has stats or effects that need to be scaled
 func (s Spell) auraEffectNeedsScaled() bool {
 	if s.EffectAura1 == 0 {
 		return false
@@ -132,49 +231,50 @@ func (s Spell) HasAuraEffect() bool {
 	return s.EffectAura1 != 0 || s.EffectAura2 != 0 || s.EffectAura3 != 0
 }
 
-// If a spell effect can be converted over to a primary stat addition
-func EffectCanBeConv(effect int) bool {
-	statMods := [...]int{17, 10, 22, 23, 58}
+func AuraEffectCanBeConv(effect int) bool {
+	statMods := [...]int{8, 13, 22, 34, 85, 99, 124, 135, 189}
 	return funk.Contains(statMods, effect)
 }
 
-func AuraEffectCanBeConv(effect int) bool {
+// Lookup details about the effect and return the stat type -1 indicates not found
+func convertAuraEffect(effect int) int {
+	if !funk.Contains(AuraEffectsStatMap, effect) {
+		log.Printf("effect %v not found in SpellEffectStatMap skipping", effect)
+		return -1
+	}
 
-	statMods := [...]int{13, 22, 34, 85, 99, 107, 115, 124, 135, 189}
-	return funk.Contains(statMods, effect)
+	return AuraEffectsStatMap[effect]
 }
 
 func (s Spell) ConvertToStats() ([]SpellStat, error) {
 	stats := []SpellStat{}
 
 	if s.Effect1 == 0 && s.EffectAura1 == 0 {
-		fmt.Print("Spell does not have an effect1 or autaEffect1")
-		return stats, nil
+		return stats, fmt.Errorf("spell does not have an effect1 or auraEffect1")
 	}
 
-	if EffectCanBeConv(s.Effect1) {
+	effects := s.GetAuraEffects()
+	for _, e := range effects {
+		if !AuraEffectCanBeConv(e.Effect) {
+			continue
+		}
+
+		statId := convertAuraEffect(e.Effect)
+		if statId == -1 {
+			continue
+		}
+
 		stats = append(stats, SpellStat{
-			StatType:  SpellEffectStatMap[s.Effect1][0],
-			StatValue: s.EffectBasePoints1,
-			Budget:    s.EffectBonusMultiplier1,
+			StatType:  statId,
+			StatValue: e.CalculatedMax,
+			Budget:    0,
 		})
 	}
 
-	if s.Effect2 != 0 && EffectCanBeConv(s.Effect2) {
-		stats = append(stats, SpellStat{
-			StatType:  SpellEffectStatMap[s.Effect2][0],
-			StatValue: s.EffectBasePoints2,
-			Budget:    s.EffectBonusMultiplier2,
-		})
-	}
+	statId = convertAuraEffect(s.EffectAura1)
 
-	if s.Effect3 != 0 && EffectCanBeConv(s.Effect3) {
-		stats = append(stats, SpellStat{
-			StatType:  SpellEffectStatMap[s.Effect3][0],
-			StatValue: s.EffectBasePoints3,
-			Budget:    s.EffectBonusMultiplier3,
-		})
-	}
+	// spell effects first
+	// effects := s.GetSpellEffects()
 
 	return stats, nil
 }
